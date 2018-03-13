4cbb79ba30ba4d73ca3e3f74f550a62969f32002
root <root@ip-172-31-53-63.ec2.internal>
2018-03-13 06:00:48 +0000

    initial commit

---
+++ lambda_function.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+
+
+import boto3
+import time
+import json
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+    message = event['Records'][0]['Sns']['Message']
+    print("From SNS: " + message)
+    instanceId = 'i-04abda8a48008dca8'
+    if (message=='start') :
+        ssmClient = boto3.client('ssm')
+        ssmCommand = ssmClient.send_command(
+        InstanceIds = [
+           instanceId
+        ],
+        DocumentName = 'AWS-RunShellScript',
+        TimeoutSeconds = 240,
+        Comment = 'Run Pester Tests',
+        Parameters = {
+           'commands': [
+              "sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT"
+          ]
+        }
+    )
+    else :
+        ssmClient = boto3.client('ssm')
+        ssmCommand = ssmClient.send_command(
+        InstanceIds = [
+           instanceId
+        ],
+        DocumentName = 'AWS-RunShellScript',
+        TimeoutSeconds = 240,
+        Comment = 'Run Pester Tests',
+        Parameters = {
+           'commands': [
+             "sudo iptables -I INPUT -p tcp --dport 80 -j DROP"
+          ]
+        }
+    )
+    print("From SNS: " + message)
